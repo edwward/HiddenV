7b2ac4188ecc1376a06e32e9a41528c63ab91b40
Eduard Feuereis <edwward@seznam.cz>
2022-11-19 09:38:30 +0100

    Added two controllers, added new models

---
+++ HiddenVilla_Api/Controllers/HomeRoomController.cs
@@ -0,0 +1,56 @@
+﻿using Business.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Models;
+
+namespace HiddenVilla_Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeRoomController : Controller
+    {
+        private readonly IHotelRoomRepository _hotelRoomRepository;
+
+        public HomeRoomController(IHotelRoomRepository hotelRoomRepository)
+        {
+            _hotelRoomRepository = hotelRoomRepository;
+        }
+
+        //zakladni httpget endpoint, vraci vsechny rooms z db
+        [HttpGet]
+        public async Task<IActionResult> GetHotelRooms()
+        {
+            var allRooms = await _hotelRoomRepository.GetAllHotelRooms();
+            return Ok(allRooms);
+        }
+
+
+        //endpoint pro individualni room, pokud je roomId null, vrat error
+        [HttpGet("{roomId}")]
+        public async Task<IActionResult> GetHotelRoom(int? roomId)
+        {
+            if (roomId == null)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    Title = "Error",
+                    ErrorMessage = "Invalid Room Id",
+                    StatusCode = StatusCodes.Status400BadRequest
+                });  
+            }
+
+            var roomDetails = await _hotelRoomRepository.GetHotelRoom(roomId.Value);
+
+            if (roomDetails == null)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    Title = "Error",
+                    ErrorMessage = "Invalid Room Id",
+                    StatusCode = StatusCodes.Status404NotFound
+                });
+            }
+
+            return Ok(roomDetails);
+        }
+    }
+}
--- HiddenVilla_Api/Program.cs
+++ HiddenVilla_Api/Program.cs
@@ -8,23 +8,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//stejny db context jako v hiddenvilla_server projektu
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+//stejny applicationuser service jako identityuser
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 
+//stejny automapper i repositories jako v hiddenvilla_server projektu
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IHotelRoomRepository, HotelRoomRepository>();
 builder.Services.AddScoped<IAmenityRepository, AmenityRepository>();
 builder.Services.AddScoped<IHotelImagesRepository, HotelImagesRepository>();
-
-builder.Services.AddRouting(options => options.LowercaseQueryStrings = true);
 
 var app = builder.Build();
 
