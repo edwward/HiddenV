800d45773491337080e9a922aeaff67c072af1df
Eduard Feuereis <edwward@seznam.cz>
2022-11-20 06:18:40 +0100

    DTOs for user registration

---
+++ HiddenVilla_Api/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HiddenVilla_Api.Controllers
+{
+    //for sign in and out funkcionalitu
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager, 
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
+    }
+}
---
+++ Models/RegistrationResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    //pouziti k atributum ktere user bude zadavat pri registraci na webu
+    public class RegistrationResponseDTO
+    {
+        public bool IsRegistrationSuccessful { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
