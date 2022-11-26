58090b6141680dc07615ff546896a3cb1ad2641d
Eduard Feuereis <edwward@seznam.cz>
2022-11-26 19:52:55 +0100

    Added toastr js

---
+++ HiddenVilla_Client/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+
+namespace HiddenVilla_Client.Helper
+{
+    //extension metody pro JS Runtime
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message) 
+        {
+            //showtoastr je nazev js metody ke spusteni toastr (viz common.js), success je parametr k toastru
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        
+        public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message) 
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
