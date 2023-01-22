6a78404cfca6e398f404bb23a6f9a6f03a658d35
Eduard Feuereis <edwward@seznam.cz>
2023-01-22 16:22:31 +0100

    Changes in index.razor in client project

---
+++ HiddenVilla_Client/Model/ViewModel/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿namespace HiddenVilla_Client.Model.ViewModel
+{
+    //view model pro index.html
+    public class HomeVM
+    {
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public DateTime EndDate { get; set; }
+        public int NumberOfNights { get; set; } = 1;
+    }
+}
